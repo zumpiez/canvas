7c8fbd47b1e623caecf3c8c78f7a6b8c2bf53292
Colin Teal <the30yearswar@hotmail.com>
2011-07-13 13:26:53 -0700

    Semicolons

--- utils.js
+++ utils.js
@@ -7,17 +7,17 @@ utils.log = function (message) {
     if (!!console) {
         console.log(message);
     }
-}
+};
 
 //given a fully qualified object name, insures that each object in the chain is instantiated.
 //this function will not destroy any objects that already exist.
 utils.namespace = function (namespace) {
-    var names = namespace.split("."), lastCreated = window
+    var names = namespace.split("."), lastCreated = window;
     utils.each(names, function (name, index, args) {
         lastCreated[name] = lastCreated[name] || {}; //leave it alone if it exists, or create it now
         lastCreated = lastCreated[name];
-    }, lastCreated)
-}
+    }, lastCreated);
+};
 
 //calls the passed function for each item in an array
 //function is called with the arguments (item, index, [args])
@@ -26,7 +26,7 @@ utils.each = function (array, fn, args) {
     for (i = 0; i < length; i++) {
         fn(array[i], i, args);
     }
-}
+};
 
 // attach an event handler to a DOM element in a way that works in IE<9 as well as real browsers
 utils.on = function (el, event, fn) {
@@ -35,4 +35,4 @@ utils.on = function (el, event, fn) {
     } else {
         el.attachEvent("on" + event, fn);
     }
-}
+};
