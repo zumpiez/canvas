0f0b5c959ba9d4d46609b07f3d93701589a95064
Colin Teal <the30yearswar@hotmail.com>
2011-07-13 15:19:31 -0700

    Making things more interesting

---
+++ entity.js
@@ -0,0 +1,13 @@
+function Entity(options) {
+    options = options || {};
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+}
+
+Entity.prototype.update = function (gametime) {
+    return this;
+};
+
+Entity.prototype.draw = function (context) {
+    return this;
+};
