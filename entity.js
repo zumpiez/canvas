32d3fb1da81746cbd2b2ada0da14167a4a5b8e8c
Colin Teal <the30yearswar@hotmail.com>
2011-07-15 21:01:29 -0700

    Fixing a few style issues

--- entity.js
+++ entity.js
@@ -7,14 +7,14 @@ function Entity(options) {
     // continue the inheritance
     utils.inherit(this, Container);
 
-    if(!!options.translation) {
+    if (!!options.translation) {
         this.translation = options.translation;
     } else if (!!options.x || !!options.y) {
         this.translation = new Vector2(options.x, options.y);
     } else {
         this.translation = Vector2.zero();
     }
-};
+}
 
 Entity.prototype.update = function (gametime) {
     return this;
