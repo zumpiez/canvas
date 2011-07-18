a80ddd02ae5c739871bfb5944ae7732b928ee729
Colin Teal <the30yearswar@hotmail.com>
2011-07-17 18:27:51 -0700

    Fold Container into Entity

--- entity.js
+++ entity.js
@@ -1,15 +1,11 @@
 Cale.require("vector2");
 Cale.require("component");
-Cale.require("container");
-
-// setup inheritance
-Cale.inherit(Cale.Entity, Cale.Container);
 
 Cale.Entity = function (options) {
-    options = options || {};
+    // private variable for storing components
+    var components = [];
 
-    // continue the inheritance
-    Cale.inherit(this, Cale.Container, options);
+    options = options || {};
 
     if (!!options.translation) {
         this.translation = options.translation;
@@ -18,6 +14,53 @@ Cale.Entity = function (options) {
     } else {
         this.translation = Cale.Vector2.zero();
     }
+
+     // add a component
+    this.addComponent = function (component) {
+        // set the container of the component to this
+        component.container = this;
+        // add the component to the list
+        components.push(component);
+        // for the chaining
+        return this;
+    };
+
+    // remove a component
+    this.removeComponent = function (component) {
+        var length = components.length;
+        // iterate over the components
+        while (length--) {
+            // is this the right component to remove?
+            if (component === components[length]) {
+                // get it out of here
+                components.splice(length, 1);
+                // we removed it
+                return true;
+            }
+        }
+        // we didn't remove it
+        return false;
+    };
+
+    // remove all components
+    this.removeAllComponents = function () {
+        // kill all components
+        components = [];
+        // for the chaining
+        return this;
+    };
+
+    // send a message to the components
+    this.send = function (message) {
+        var length = components.length;
+        // iterate over the components
+        while (length--) {
+            // notify
+            components[length].receive(message);
+        }
+        // for the chaining
+        return this;
+    };
 };
 
 Cale.Entity.prototype.update = function (gametime) {
