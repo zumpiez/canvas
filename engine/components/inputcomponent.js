bd59d66e6877aae10b8f7b68716cf26adaa0e7e1
Colin Teal <the30yearswar@hotmail.com>
2011-07-23 07:27:39 +0000

    Comments

--- engine/components/inputcomponent.js
+++ engine/components/inputcomponent.js
@@ -1,21 +1,28 @@
+// require input and component
 Cale.require(["system/input", "engine/component"], function () {
+    // essentially a static class variable
     var keyboard = Cale.Input.Keyboard.getInstance();
-
+    // set up the inheritance
     Cale.inherit(Cale.InputComponent, Cale.Component);
-
+    // define the input component
     Cale.InputComponent = function() {
+        // continue the inheritance
         Cale.inherit(this, Cale.Component);
-
+        // override the receive function of component
         this.receive = function (message) {
+            // sanitize message a bit
             message = message || {};
+            // verify that we are the intended recipient
             if (message.type === "input") {
+                // call update with the container, and keyboard
                 this.update(this.container(), keyboard);
                 return true;
             }
             return false;
         };
     };
-
+    
+    // virtual method for update
     Cale.InputComponent.prototype.update = function (container, keyboard) {
 
     };
