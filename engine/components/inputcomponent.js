55ede1ce46255e3ec1e9df234bd45dd56226f644
Colin Teal <the30yearswar@hotmail.com>
2011-07-24 01:40:14 -0700

    Remove fat

--- engine/components/inputcomponent.js
+++ engine/components/inputcomponent.js
@@ -4,18 +4,7 @@ Cale.require(["system/input", "engine/component"], function () {
     Cale.InputComponent = function() {
         // continue the inheritance
         Cale.inherit(this, Cale.Component);
-        // override the receive function of component
-        this.keyboard = Cale.Input.Keyboard.getInstance();
-        this.receive = function (message) {
-            // sanitize message a bit
-            message = message || {};
-            // verify that we are the intended recipient
-            if (message.type === "input") {
-                // call update with the container, and keyboard
-                this.update(this.container(), keyboard);
-                return true;
-            }
-            return false;
-        };
     };
+
+    Cale.InputComponent.prototype.keyboard = Cale.Input.Keyboard.getInstance;
 });
