2743095fd17a191cfdaa6d170beadbd0f5dfebe8
Colin Teal <the30yearswar@hotmail.com>
2011-07-26 02:19:46 -0700

    Refactor and expansion

--- system/graphics.js
+++ system/graphics.js
@@ -1,5 +1,5 @@
 Cale.Graphics = function (options) {
-    var self = this, type, canvas, context;
+    var type, canvas, context;
 
     options = options || {};
 
@@ -30,69 +30,124 @@ Cale.Graphics = function (options) {
         }
         return context;
     };
+};
 
     // get or set the height
-    this.height = function(height) {
-        if (!!height) {
-            // force height to a number
-            canvas.height = +height;
-            return self;
-        } else {
-            // treat the call as a getter
-            return canvas.height;
-        }
-    };
+Cale.Graphics.prototype.height = function(height) {
+    if (!!height) {
+        // force height to a number
+        this.canvas().height = +height;
+        return this;
+    } else {
+        // treat the call as a getter
+        return this.canvas().height;
+    }
+};
 
     // get or set the width
-    this.width = function(width) {
-        if (!!width) {
-            // force width to a number
-            canvas.width = +width;
-            // return self for chaining
-            return self;
-        } else {
-            // treat the call as a getter
-            return canvas.width;
-        }
-    };
+Cale.Graphics.prototype.width = function(width) {
+    if (!!width) {
+        // force width to a number
+        this.canvas().width = +width;
+        // return this for chaining
+        return this;
+    } else {
+        // treat the call as a getter
+        return this.canvas().width;
+    }
+};
 
-    // clear the canvas of everything
-    this.clear = function() {
-        canvas.width = canvas.width;
-        return self;
-    };
+// clear the canvas of everything
+Cale.Graphics.prototype.clear = function() {
+    this.width(this.width());
+    return this;
+};
 
-    // write some text
-    this.text = function(options) {
-        var context = self.context();
-        options = options || {};
-        context.fillText(options.text, options.x, options.y);
-        return self;
-    };
+// write some text
+Cale.Graphics.prototype.text = function(options) {
+    var context = this.context();
+    options = options || {};
+    context.fillText(options.text, options.x, options.y);
+    return this;
+};
 
-    // draw a circle
-    this.arc = function(options) {
-        var context = self.context();
-        options = options || {};
-        context.beginPath();
-        context.arc(options.x, options.y, options.radius,
-            options.startAngle || 0, options.endAngle || Math.PI * 2,
-            options.anticlockwise || false);
-        context.closePath();
-        return self;
-    };
+// draw a circle
+Cale.Graphics.prototype.arc = function(options) {
+    var context = this.context();
+    options = options || {};
+    context.beginPath();
+    context.arc(options.x, options.y, options.radius,
+        options.startAngle || 0, options.endAngle || Math.PI * 2,
+        options.anticlockwise || false);
+    context.closePath();
+    return this;
+};
 
-    // fill
-    this.fill = function() {
-        var context = self.context();
-        context.fill();
-        return self;
-    };
+// fill
+Cale.Graphics.prototype.fill = function() {
+    var context = this.context();
+    context.fill();
+    return this;
+};
 
-    // stroke
-    this.stroke = function() {
-        var context = self.context();
-        context.stroke();
-        return self;
-    };
+// stroke
+Cale.Graphics.prototype.stroke = function() {
+    var context = this.context();
+    context.stroke();
+    return this;
+};
+
+// push the last canvas transformation
+Cale.Graphics.prototype.save = function () {
+    var context = this.context();
+    context.save();
+    return this;
+};
+
+// pop the last saved canvas transformation
+Cale.Graphics.prototype.restore = function () {
+    var context = this.context();
+    this.context().restore();
+    return this;
+};
+
+// scale the canvas
+Cale.Graphics.prototype.scale = function (options) {
+    var context = this.context();
+    options = options || {};
+    context.scale(options.x, options.y);
+    return this;
+};
+
+// rotate by an angle around the origin
+Cale.Graphics.prototype.rotate = function (angle) {
+    var context = this.context();
+    context.rotate(angle);
+    return this;
+};
+
+// set the translation
+Cale.Graphics.prototype.translate = function (options) {
+    var context = this.context();
+    options = options || {};
+    context.translate(options.x, options.y);
+    return this;
+};
+
+// changes the transformation matrix to apply the matrix
+Cale.Graphics.prototype.transform = function (options) {
+    var context = this.context();
+    options = options || {};
+    context.transform(options.a, options.b, options.c,
+        options.d, options.e, options.f);
+    return this;
+};
+
+// changes the transformation matrix to the matrix
+Cale.Graphics.prototype.setTransform = function (options) {
+    var context = this.context();
+    options = options || {};
+    context.setTransform(options.a, options.b, options.c,
+        options.d, options.e, options.f);
+    return this;
 };
