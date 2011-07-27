610c628058c8674cd7d69a58eac1292203a63e1d
Colin Teal <the30yearswar@hotmail.com>
2011-07-26 17:03:36 -0700

    Convenience

--- engine/component.js
+++ engine/component.js
@@ -7,6 +7,54 @@ Cale.Component.prototype.initialize = function() {
     //override me.
 };
 
+// publish to parent topic
+Cale.Component.prototype.publish = function (options) {
+    var topic, message;
+
+    if (!!this.parent) {
+        options = options || {};
+
+        if (arguments.length > 1) {
+            topic = options;
+            message = arguments[1];
+        } else {
+            topic = options.topic;
+            message = options.message;
+        }
+
+        this.parent.publish(topic, message);
+    }
+
+    return this;
+};
+
+// subscribe to parent topic
+Cale.Component.prototype.subscribe = function (options) {
+    var scope = this, topic, callback;
+
+    if (!!this.parent) {
+        options = options || {};
+
+        if (arguments.length > 1) {
+            topic = options;
+            callback = arguments[1];
+            if (arguments.length > 2) {
+                scope = arguments[2];
+            }
+        } else {
+            topic = options.topic;
+            callback = options.callback;
+            scope = options.scope || scope;
+        }
+
+        this.parent.subscribe(topic, function (message) {
+            callback.call(scope, message);
+        });
+    }
+
+    return this;
+};
+
 Cale.Component.create = function (options) {
     var component = new Cale.Component();
     if (Cale.isObject(options) || Cale.isFunction(options)) {
